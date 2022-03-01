e5981dc429d2c10fbb1676a08cc1b1f01b9a23c8
AravindanAnbarasu <aravindan19675@gmail.com>
2022-03-01 20:38:29 +0100

    Solution to task2

--- src/2021/SyntaxScoring/src/SyntaxScoring/SyntaxScoring.cpp
+++ src/2021/SyntaxScoring/src/SyntaxScoring/SyntaxScoring.cpp
@@ -1,16 +1,20 @@
 // Syntax_Scoring.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
+#include <algorithm>
 #include <fstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-auto analyse_line(string &line) //find first illegal character
+pair<long long, long long> analyse_line(string& line) //find first illegal character
 {
 	std::vector<char> v;
-	auto syntax_score = 0LL; 
+	auto syntax_score = 0LL;
+	auto total_score = 0LL;
+	string s = "([{<";
+	pair<long long, long long> pair;
 
 	for (auto c : line)
 	{
@@ -49,25 +53,79 @@ auto analyse_line(string &line) //find first illegal character
 			break;
 
 		v.pop_back(); // remove the item if no error
+
+	}
+	while (!v.empty())  //we have all the open brackets without a pair in the vector
+	{
+		total_score = total_score * 5 + s.find(v.back()) + 1;
+		v.pop_back();
 	}
 
-	return syntax_score;
-} 
+	pair.first = syntax_score;
+	pair.second = total_score;
+
+	return pair;
+}
+
+/*long long part2(string &line) //find first illegal character
+{
+	std::vector<char> v;
+	auto total_score = 0LL;
+
+	string s = "([{<";
+
+	for (auto c : line)
+	{
+		switch (c)
+		{
+		case '{':
+		case '<':
+		case '[':
+		case '(':
+			v.push_back(c);
+			continue;
+		}
+
+		v.pop_back(); // remove the item if no error
+	}
+
+	while (!v.empty())  //we have all the open brackets without a pair in the vector
+	{
+		total_score = total_score * 5 + s.find(v.back()) + 1;
+		v.pop_back();
+	}
+
+	return total_score;
+}*/
+
 
 int main()
 {
 	string line;
 	auto answer = 0LL;
-
+	vector<long long> total_scores;
+	auto answer2 = 0LL;
 
 	std::ifstream file("input_syntax.txt"); //load the file
 
 	while (getline(file, line)) //read each line   
 	{
 		auto result = analyse_line(line);
-		answer += result;
+		answer += result.first;
+
+		if (result.first == 0)
+			total_scores.push_back(result.second);
 	}
-		
-	cout << "Answer:" << answer;
+
+	cout << "Answer:" << answer << endl;
+
+	sort(total_scores.begin(), total_scores.end());
+
+	//cout << "Size:" << total_scores.size() << endl;
+
+	answer2 = total_scores[total_scores.size() / 2];
+
+	cout << "Answer2:" << answer2 << endl;
+
 }
 
