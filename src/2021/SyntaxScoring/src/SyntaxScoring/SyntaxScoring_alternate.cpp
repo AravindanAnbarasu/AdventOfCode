c6abed245c3586c2c94f1577a626760082dfd947
AravindanAnbarasu <aravindan19675@gmail.com>
2022-03-08 21:06:27 +0100

    code allignment fix

--- src/2021/SyntaxScoring/src/SyntaxScoring/SyntaxScoring_alternate.cpp
+++ src/2021/SyntaxScoring/src/SyntaxScoring/SyntaxScoring_alternate.cpp
@@ -10,35 +10,37 @@
 
 using namespace std;
 
-pair<long long, long long> analyse_line(string& line)
+pair<long long, long long> analyse_line(string &line)
 {
     pair<long long, long long> pair;
     vector<char> v;
     vector<char> opening_braces = {'(', '[', '{', '<'};
-    unordered_map<char, char> m = {{')', '('},{']', '['},{'}', '{'},{'>', '<'},{'(', ')'},{'[', ']'},{'{', '}'},{'<', '>'}};
-    unordered_map<char, long long> syntax_scr = {{')', 3LL},{']', 57LL}, {'}', 1197LL}, {'>', 25137LL}};
+    unordered_map<char, char> m = {{')', '('}, {']', '['}, {'}', '{'}, {'>', '<'}, {'(', ')'}, {'[', ']'}, {'{', '}'}, {'<', '>'}};
+    unordered_map<char, long long> syntax_scr = {{')', 3LL}, {']', 57LL}, {'}', 1197LL}, {'>', 25137LL}};
 
-    unordered_map<char, long long> score = {{')', 1LL},{']', 2LL},{'}', 3LL},{'>', 4LL}};
+    unordered_map<char, long long> score = {{')', 1LL}, {']', 2LL}, {'}', 3LL}, {'>', 4LL}};
 
     auto syntax_score = 0LL;
     auto total_score = 0LL;
 
-    for (auto c: line) {
+    for (auto c : line)
+    {
 
-        auto itr = find(opening_braces.begin(), opening_braces.end(),c);
-        if (itr!= opening_braces.end())
+        auto itr = find(opening_braces.begin(), opening_braces.end(), c);
+        if (itr != opening_braces.end())
             v.push_back(c);
 
         else if (v.back() == m[c])
             v.pop_back();
 
-        else{  // if first illegal character found break the for loop
+        else
+        { // if first illegal character found break the for loop
             syntax_score = syntax_scr[c];
             break;
         }
     }
 
-    while (!v.empty())  //we have all the open brackets without a pair in the vector
+    while (!v.empty()) // we have all the open brackets without a pair in the vector
     {
         total_score = 5 * total_score + score[m[v.back()]];
         v.pop_back();
@@ -50,35 +52,31 @@ pair<long long, long long> analyse_line(string& line)
     return pair;
 }
 
-
 int main()
 {
-	string line;
-	auto answer = 0LL;
-	vector<long long> total_scores;
-	auto answer2 = 0LL;
+    string line;
+    auto answer = 0LL;
+    vector<long long> total_scores;
+    auto answer2 = 0LL;
 
-    std::ifstream file("input_syntax.txt"); //load the file
+    std::ifstream file("input_syntax.txt"); // load the file
 
-	while (getline(file, line)) //read each line   
-	{
+    while (getline(file, line)) // read each line
+    {
         auto result = analyse_line(line);
-		answer += result.first;
+        answer += result.first;
 
-		if (result.first == 0)
-			total_scores.push_back(result.second);
-	}
+        if (result.first == 0)
+            total_scores.push_back(result.second);
+    }
 
     cout << "Answer:" << answer << endl;
 
-	sort(total_scores.begin(), total_scores.end());
+    sort(total_scores.begin(), total_scores.end());
 
-	//cout << "Size:" << total_scores.size() << endl;
+    // cout << "Size:" << total_scores.size() << endl;
 
-	answer2 = total_scores[total_scores.size() / 2];
+    answer2 = total_scores[total_scores.size() / 2];
 
-	cout << "Answer2:" << answer2 << endl;
-
+    cout << "Answer2:" << answer2 << endl;
 }
-
-
